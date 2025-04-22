eb7fc34fb8aab1bb6d49ba81a4073a511e6e3417
fengchuikudangpipiliang <167202813+fengchuikudangpipiliang@users.noreply.github.com>
2025-04-22 08:22:36 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace NET_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"D:\code\c#\test.txt";
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+            for (int i = 0; i < 20; i++)
+            {
+                fileStream.WriteByte((byte)i);
+            }
+            fileStream.Position = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(fileStream.ReadByte() + " ");
+            }
+
+        }
+    }
+}
