163a113716084a72301dd959ea106e983c46df8b
fengchuikudangpipiliang <167202813+fengchuikudangpipiliang@users.noreply.github.com>
2025-04-24 14:38:54 +0800

    在调用方法中同步等待任务--wait

--- Program.cs
+++ Program.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Timers;
+
 namespace NET_Core
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string fileName = @"D:\code\c#\test.txt";
-            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
-            for (int i = 0; i < 20; i++)
-            {
-                fileStream.WriteByte((byte)i);
-            }
-            fileStream.Position = 0;
-            for (int i = 0; i < 20; i++)
-            {
-                Console.Write(fileStream.ReadByte() + " ");
-            }
+            Task<int> task1 = CountCharacter(1,"http://www.illustratedcsharp.com");
+            Task<int> task2 = CountCharacter(2,"https://www.csdn.net");
+            //如果是task1.wait(),那么就是用于单一的task对象，等待task1完成再执行下面的代码
+            Task[] tasks = { task1,task2};
+            Task.WaitAll(tasks);
+            Console.WriteLine($"task1 {(task1.IsCompleted?" ":"not")} complete");
+            Console.WriteLine($"task2 {(task2.IsCompleted ? " " : "not")} complete");
+            Console.WriteLine($"The count is {task1.Result}and {task2.Result}");
 
+        }
+        public static async Task<int> CountCharacter(int id,string url)
+        {
+            string site=await new HttpClient().GetStringAsync(url);
+            return site.Length; 
         }
     }
 }
