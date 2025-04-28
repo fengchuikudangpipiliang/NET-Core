95b0d7ba1513be39e73d68270d9bc01c927fd2ab
fengchuikudangpipiliang <167202813+fengchuikudangpipiliang@users.noreply.github.com>
2025-04-28 16:18:54 +0800

    实现一个由依赖注入实现的邮件发送模拟系统

--- Program.cs
+++ Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 using System.Text;
 using System.Timers;
-
+using Microsoft.Extensions.DependencyInjection;
+#pragma warning disable CRR0029
 namespace NET_Core
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            StringBuilder stringBuilder = new StringBuilder();
-            for(int i = 0; i <10000; i++)
-            {
-                stringBuilder.Append("asdfasdfasfdasdf");
-            }
-            //反正await等待期间，这个线程用不了
-            //所以.NET就把这个线程返回到线程池，直到这个异步方法执行完毕，再取出一个线程来执行接下来的代码
-            //优化：如果等待时间很短，就没必要切换线程
-            await File.WriteAllTextAsync(@"D:\code\c#\hhhh.txt", stringBuilder.ToString());
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+           Test t = new DerivedClass();
+            t.Test1();
         }
+    }
+    interface IController
+    {
+        void Test();
+    }
+    class Controller 
+    {
+        private readonly ILog _log;
+        public Controller(ILog log)
+        {
+            this._log = log;    
+        }
+        public void Test()
+        {
+            this._log.MyLog("sss");
+        }
+    }
+    interface ILog
+    {
+        public void MyLog(string message);
+    }
+    class LogIm : ILog
+    {
+        public void MyLog(string message)
+        {
+            Console.WriteLine("hello"+message);
+        }
+    }
+    class Test
+    {
+        public void Test1()
+        {
+            Console.WriteLine("test");
+        }
+    }
+    class DerivedClass:Test
+    {
+        public void Test1 (){ Console.WriteLine("继承"); }
     }
 }
