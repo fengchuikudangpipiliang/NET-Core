3cd211e6737c8490fe0f4536d0178d0613fac972
fengchuikudangpipiliang <167202813+fengchuikudangpipiliang@users.noreply.github.com>
2025-04-29 19:12:22 +0800

    使用NLog,配置NLog.config,实现日志用console provider和fiile provider输出日志

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Timers;
-using Microsoft.Extensions.DependencyInjection;
-using LitJson;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 #pragma warning disable CRR0029
 namespace NET_Core
 {
@@ -13,56 +9,21 @@ namespace NET_Core
         static async Task Main(string[] args)
         {
             ServiceCollection services = new ServiceCollection();
+            services.AddLogging(e => {
+                e.AddConsole();
+                e.AddEventLog();
+                e.AddNLog();
+                e.SetMinimumLevel(LogLevel.Warning);
+            });
+            services.AddScoped<Test>();
 
-            services.AddScoped<TestController>();
-            services.AddScoped<TestController1>();
-            
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-           
-            builder.AddJsonFile("config.json",optional:false,true);
-            
-            IConfigurationRoot configRoot = builder.Build();
-
-            services.AddOptions().Configure<Config>(e=>configRoot.Bind(e)).Configure<Proxy>(e=>configRoot.GetSection("proxy").Bind(e));
-            using (var sp = services.BuildServiceProvider())
+            using(var sp = services.BuildServiceProvider())
             {
-                
-                while (true)
-                {
-                    using (var sc = sp.CreateScope())
-                    {
-                        TestController t = sc.ServiceProvider.GetService<TestController>();
-                        TestController1 t1 =  sc.ServiceProvider.GetRequiredService<TestController1>();
-                        Console.WriteLine("这是新的一个scope");
-                        t.Test();
-                        t1.Test();
-                    }
-                    Console.WriteLine("按下任意键继续");
-                    Console.ReadKey();
-                }
+                Test test = sp.GetRequiredService<Test>();
+                test.Log();
             }
-
-            string name = configRoot["name"];
-            Console.WriteLine(name);
-            string address=configRoot.GetSection("proxy:address").Value;
-            Console.WriteLine(address);
-
-            Console.WriteLine("------------------");
-            IConfigurationSection proxy=configRoot.GetSection("proxy");
-            Console.WriteLine(proxy.Exists());
-            IConfigurationSection address1 = configRoot.GetSection("proxy:address");
-            Console.WriteLine(address1.Value);
         }
     }
-    class Config
-    {
-        public string Name { get; set; }
-        public string age { get; set; }
-        public Proxy proxy;
-    }
-    class Proxy
-    {
-        public string address { get; set; }
-    }
+
    
 }
---
+++ Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Core
+{
+    public class Test
+    {
+        private readonly ILogger<Test> logger;
+        public Test(ILogger<Test> logger)
+        {
+            this.logger = logger;
+        }
+        public void Log()
+        {
+            logger.LogDebug("开始登录无限火力");
+            logger.LogWarning("登录失败..");
+        }
+    }
+}
